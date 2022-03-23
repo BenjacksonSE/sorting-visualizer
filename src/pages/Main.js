14690bb7c5080462e6a836764efaae08fe6f3e6a
Ben Jackson <benjacksonSE@gmail.com>
2022-03-23 19:26:07 -0400

    Added navbar control handeler to main class

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,4 @@
-import { toHaveDisplayValue } from '@testing-library/jest-dom/dist/matchers';
-import React, { Component } from 'react'
+import React from 'react'
 import Interface from '../components/Interface/Interface';
 import Sorts from '../components/Sorts';
 import { newRandomArr } from '../assets/helpers';
@@ -13,10 +12,24 @@ export default class Main extends React.Component {
     working: false,
     arr: [],
   }
-
-
+  //Navbar controls
+  onChange = (value, selection) => {
+    if(selection === "algo" && !this.state.working) {
+        this.setState({ algorithm: Number(value) });
+    }
+    else if(selection === "speed") {
+        this.setState({ speed: Number(value) });
+    }
+    else if(selection === "size" && !this.state.working) {
+        this.setState({ size: Number(value) });
+        this.generateArr();
+    }
+  };
+  //Generate new Arr on page load
+  componentDidMount(){
+    this.generateArr();
+  }
   //Generate new list based on arr length
-  
   generateArr = (value=0) => {
     if((this.state.arr.length !== this.state.arrLeng && !this.state.working) || Number(value)===1){
       let newArr = newRandomArr(this.state.arrLeng);
