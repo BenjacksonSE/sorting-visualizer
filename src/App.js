67639ed6c780c8151d01391af647788875215579
Ben Jackson <benjacksonSE@gmail.com>
2022-03-23 18:26:05 -0400

    Set up main for working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Interface from './components/Interface/Interface';
+import Main from './pages/Main';
 
 function App() {
   return (
     <div className="App">
       <Interface />
+      <Main />
     </div>
   );
 }
