8be66d06a5391f2cfbdead7b655ff2b7721cc10c
Ben Jackson <benjacksonSE@gmail.com>
2022-03-23 10:36:19 -0400

    Added bubble sort for testing

---
+++ src/assets/algorithms/Bubblesort.js
@@ -0,0 +1,20 @@
+export const bubbleSort = async(array, length) => {
+    const swap = async (array, index1, index2) => {
+      let temp = array[index1];
+      array[index1] = array[index2];
+      array[index2] = temp;
+    };
+    let moves = [];
+    for(let i = 0 ; i < length-1 ; ++i) {
+        for(let j = 0 ; j < length-i-1 ; ++j) {
+            if(array[j] > array[j+1]) {
+                await swap(array, j, j+1);
+                moves.push([j, j+1, 1]);
+            }
+            else {
+                moves.push([j, j+1, !1]);
+            }
+        }
+    }
+    return moves;
+};
