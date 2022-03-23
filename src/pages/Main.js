ce4ad559dbb0b3b1c283daef1b3652a002e68cb1
Ben Jackson <benjacksonSE@gmail.com>
2022-03-23 18:28:11 -0400

    Defined local states in main class

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react'
 
 export default class Main extends React.Component {
+  state = {
+    arrLeng: 20,
+    speed: 1,
+    algo: 1,
+    working: false,
+    list: [],
+  }
+
+
+
   render() {
     return (
       <div>Main</div>
