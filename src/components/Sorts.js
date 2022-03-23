a0a422c65dbfdc662ff6f005378aeb6e02d8cdfc
Ben Jackson <benjacksonSE@gmail.com>
2022-03-23 18:24:14 -0400

    Added in logic to change colors and height of elements based on is working on or not

--- src/components/Sorts.js
+++ src/components/Sorts.js
@@ -1,11 +1,16 @@
 import React from 'react'
 
 const Sorts = ({props}) => {
+  const forClassChange = (val) => {
+      if(val === 0) return 'elem';
+      else if(val === 1) return 'elem working';
+      return 'elem sorted';
+  }
   return (
     <div className='sorts'>
       <div className='array'>
         {props.list.map((elem, index) => (
-          <div className={forClassChange(element.whichClass)} key={index} value={element.key}/>
+          <div className={forClassChange(elem.whichClass)} key={index} value={elem.key} style={{height:`${4*elem.key}px`}}/>
         ))}
       </div>
 
