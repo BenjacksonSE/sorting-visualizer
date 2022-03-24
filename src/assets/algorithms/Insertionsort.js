563a3f69cc456393882753f9ad63386b2d7e3f4f
Ben Jackson <benjacksonSE@gmail.com>
2022-03-24 17:07:18 -0400

    Added insertion sort

--- src/assets/algorithms/Insertionsort.js
+++ src/assets/algorithms/Insertionsort.js
@@ -1,12 +1,14 @@
 // insertion sort returns container with 2 indexes and boolean
 // describing to swap or not
 export const insertionSort = async(array, length) => {
-  const swapElems = async (array, index1, index2) => {
-    let temp = array[index1];
-    array[index1] = array[index2];
-    array[index2] = temp;
-  };
     let moves = [];
+
+    const swapElems = async (array, index1, index2) => {
+      let temp = array[index1];
+      array[index1] = array[index2];
+      array[index2] = temp;
+    };
+
     for(let i = 0 ; i < length-1 ; ++i) {
         let j = i;
         while(j >= 0 && array[j] > array[j+1]) {
